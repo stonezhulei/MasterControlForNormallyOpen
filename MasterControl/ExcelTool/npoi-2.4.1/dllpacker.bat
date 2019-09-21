35313f9ebbb80530b7507a9e629f6ad52a405ed5
xin liu <xinliuchnsz@gmail.com>
2019-09-21 23:54:37 +0800

    合并npoi*.dll

---
+++ MasterControl/ExcelTool/npoi-2.4.1/dllpacker.bat
@@ -0,0 +1,19 @@
+﻿@echo off
+
+%当前目录%
+echo currDir=%CD%
+
+%工作目录%
+echo workDir=%~dp0
+
+
+cd /d %~dp0
+
+if not exist "Excel.dll" (
+%托管dll合并%
+ilmerge.exe /ndebug /target:dll /targetplatform:v4 /out:Excel.dll NPOI.dll /log ICSharpCode.SharpZipLib.dll NPOI.OOXML.dll NPOI.OpenXml4Net.dll NPOI.OpenXmlFormats.dll
+)
+
+@echo on
+
+pause
